42324590c4f2cd077725532c026910cc818e0b86
doug_bacon-admin <douglas.bacon@student.uts.edu.au>
2024-10-13 17:48:51 +1000

    updated dh paramters and modified ply files so that they can be acuratley modeled in matlab

--- @crx5/crx5.m
+++ @crx5/crx5.m
@@ -1,10 +1,5 @@
 classdef crx5 < RobotBaseClass
-    %% Linear UR3e robot based on UTS model and changed based on lecture 5 video
-    % converted to a 
-    % WARNING: This model has been created by UTS students in the subject
-    % 41013. No guarentee is made about the accuracy or correctness of the
-    % of the DH parameters of the accompanying ply files. Do not assume
-    % that this matches the real robot!
+
 
     properties(Access = public)   
         plyFileNameStem = 'crx5';
@@ -17,7 +12,7 @@ function self = crx5(baseTr)
             if nargin < 1			
 				baseTr = eye(4);				
             end
-            self.model.base = self.model.base.T * baseTr * trotx(pi/2);% rotating to have robot lay flat 
+            self.model.base = self.model.base.T * baseTr * trotx(-pi/2);% rotating to have robot lay flat 
 
             self.PlotAndColourRobot();         
         end
@@ -25,14 +20,16 @@ function self = crx5(baseTr)
 
 %% CreateModel
         function CreateModel(self)
-            link(1) = Link('d',0,'a',0,'alpha',0,'qlim',[-pi pi], 'offset',0);
-            link(2) = Link('d',0,'a',0,'alpha',0,'qlim',[-pi pi], 'offset',0);
-            link(3) = Link('d',0,'a',0,'alpha',0,'qlim',[-pi pi], 'offset',0);
-            link(4) = Link('d',0,'a',0,'alpha',0,'qlim',[-pi pi], 'offset',0);
-            link(5) = Link('d',0,'a',0,'alpha',0,'qlim',[-pi pi], 'offset',0);
-            link(6) = Link('d',0,'a',0,'alpha',0,'qlim',[-pi pi], 'offset',0);
-             
+            link(1) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-pi pi], 'offset',0);
+            link(2) = Link('d',0.2125,'a',0,'alpha',pi/2,'qlim', [-pi pi], 'offset',0);
+            link(3) = Link('d',-0.04,'a',0.405,'alpha',0,'qlim', [-pi pi], 'offset', 0);
+            link(4) = Link('d',-0.01,'a',0.42,'alpha',0,'qlim',[-pi pi],'offset', 0);
+            link(5) = Link('d',0.09,'a',0,'alpha',-pi/2,'qlim',[-pi pi], 'offset',0);
+            link(6) = Link('d',	0.155,'a',0,'alpha',0,'qlim',[-pi pi], 'offset', 0);
+            
             self.model = SerialLink(link,'name',self.name);
-        end      
+            self.model.plotopt = {'noshadow','noarrow','noshading','nowrist','nojaxes'};
+        end 
+
     end
 end
