f514380760fda9e68415c6c1212bd6df46d1f6c6
doug_bacon-admin <douglas.bacon@student.uts.edu.au>
2024-10-13 20:43:16 +1000

    Updated Dh paramaters so that it works correctly with robot.teach

--- @crx5/crx5.m
+++ @crx5/crx5.m
@@ -12,7 +12,7 @@ function self = crx5(baseTr)
             if nargin < 1			
 				baseTr = eye(4);				
             end
-            self.model.base = self.model.base.T * baseTr * trotx(-pi/2);% rotating to have robot lay flat 
+            self.model.base = self.model.base.T * baseTr * transl(0,0,0);% rotating to have robot lay flat 
 
             self.PlotAndColourRobot();         
         end
@@ -20,12 +20,12 @@ function self = crx5(baseTr)
 
 %% CreateModel
         function CreateModel(self)
-            link(1) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-pi pi], 'offset',0);
-            link(2) = Link('d',0.2125,'a',0,'alpha',pi/2,'qlim', [-pi pi], 'offset',0);
-            link(3) = Link('d',-0.04,'a',0.405,'alpha',0,'qlim', [-pi pi], 'offset', 0);
-            link(4) = Link('d',-0.01,'a',0.42,'alpha',0,'qlim',[-pi pi],'offset', 0);
-            link(5) = Link('d',0.09,'a',0,'alpha',-pi/2,'qlim',[-pi pi], 'offset',0);
-            link(6) = Link('d',	0.155,'a',0,'alpha',0,'qlim',[-pi pi], 'offset', 0);
+            % link(1) = Link('d',0,'a',0,'alpha',pi/2,'qlim',[-pi pi], 'offset',0);
+            link(1) = Link('d',0.25,'a',0,'alpha',pi/2,'qlim', [-pi pi], 'offset',0);
+            link(2) = Link('d',0.07,'a',0.405,'alpha',0,'qlim', [-pi pi], 'offset', 0);
+            link(3) = Link('d',-0.01,'a',0.42,'alpha',0,'qlim',[-pi pi],'offset', 0);
+            link(4) = Link('d',0.09,'a',0,'alpha',-pi/2,'qlim',[-pi pi], 'offset',0);
+            link(5) = Link('d',	0.155,'a',0,'alpha',0,'qlim',[-pi pi], 'offset', 0);
             
             self.model = SerialLink(link,'name',self.name);
             self.model.plotopt = {'noshadow','noarrow','noshading','nowrist','nojaxes'};
