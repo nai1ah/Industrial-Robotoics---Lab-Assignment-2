3ed24a9e112471c6a339fcf3041904349eb51c94
M-Mengineer <126317387+M-Mengineer@users.noreply.github.com>
2024-10-28 21:14:45 +1100

    added light curtain and rectangular prism creation functions

--- Robot_Base_Class.m
+++ Robot_Base_Class.m
@@ -114,6 +114,90 @@ Topbun_pos = [-0.25,-0.15,0.55];
             Lettuce = PlaceObject('lettuce.ply',Robot_Base_Class.Lettuce_pos);
             Top_Bun = PlaceObject('topbun.ply',Robot_Base_Class.Topbun_pos);
         end
+        
+        %% function to create a light curtain
+        function lightCurtain = createLightCurtain(curtainX, curtainY, curtainZ)
+            [X, Z] = meshgrid(curtainX, curtainZ); 
+            Y = curtainY * ones(size(X)); 
+            lightCurtain = surf(X, Y, Z, 'FaceColor', 'cyan', 'FaceAlpha', 0.2);
+        end
+
+        %% function to create rectangular prism
+        function [vertex,face,faceNormals] = RectangularPrism(lower,upper,plotOptions,axis_h)
+            if nargin<4
+                    axis_h=gca;
+                if nargin<3
+                    plotOptions.plotVerts=false;
+                    plotOptions.plotEdges=true;
+                    plotOptions.plotFaces=true;
+                end
+            end
+            hold on
+            
+            vertex(1,:)=lower;
+            vertex(2,:)=[upper(1),lower(2:3)];
+            vertex(3,:)=[upper(1:2),lower(3)];
+            vertex(4,:)=[upper(1),lower(2),upper(3)];
+            vertex(5,:)=[lower(1),upper(2:3)];
+            vertex(6,:)=[lower(1:2),upper(3)];
+            vertex(7,:)=[lower(1),upper(2),lower(3)];
+            vertex(8,:)=upper;
+            
+            face=[1,2,3;1,3,7;
+                 1,6,5;1,7,5;
+                 1,6,4;1,4,2;
+                 6,4,8;6,5,8;
+                 2,4,8;2,3,8;
+                 3,7,5;3,8,5;
+                 6,5,8;6,4,8];
+            
+            if 2 < nargout    
+                faceNormals = zeros(size(face,1),3);
+                for faceIndex = 1:size(face,1)
+                    v1 = vertex(face(faceIndex,1)',:);
+                    v2 = vertex(face(faceIndex,2)',:);
+                    v3 = vertex(face(faceIndex,3)',:);
+                    faceNormals(faceIndex,:) = unit(cross(v2-v1,v3-v1));
+                end
+            end
+            % If plot verticies
+            if isfield(plotOptions,'plotVerts') && plotOptions.plotVerts
+                for i=1:size(vertex,1);
+                    plot3(vertex(i,1),vertex(i,2),vertex(i,3),'r*');
+                    text(vertex(i,1),vertex(i,2),vertex(i,3),num2str(i));
+                end
+            end
+            
+            % If you want to plot the edges
+            if isfield(plotOptions,'plotEdges') && plotOptions.plotEdges
+                links=[1,2;
+                    2,3;
+                    3,7;
+                    7,1;
+                    1,6;
+                    5,6;
+                    5,7;
+                    4,8;
+                    5,8;
+                    6,4;
+                    4,2;
+                    8,3];
+            
+                for i=1:size(links,1)
+                    plot3(axis_h,[vertex(links(i,1),1),vertex(links(i,2),1)],...
+                        [vertex(links(i,1),2),vertex(links(i,2),2)],...
+                        [vertex(links(i,1),3),vertex(links(i,2),3)],'k')
+                end
+            end
+            
+            % If you want to plot the faces
+            if isfield(plotOptions,'plotFaces') && plotOptions.plotFaces
+                tcolor = [.2 .2 .8];
+                
+                patch('Faces',face,'Vertices',vertex,'FaceVertexCData',tcolor,'FaceColor','flat','lineStyle','none');
+            end
+            
+            end
         %% function to delete each brick safely through a catch function 
         function Delete_Object(object)
             try delete(object);
